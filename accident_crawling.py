75eccd3005feb149bb1eddf71190dea42d518dfb
soyeungjo <ok69531@gmail.com>
2022-07-15 17:11:23 +0900

    scrap

--- accident_crawling.py
+++ accident_crawling.py
@@ -1,7 +1,7 @@
 #%%
 import time
-import os
 import re
+import openpyxl
 import numpy as np
 import pandas as pd
 from pandas.core.indexes.base import Index
@@ -24,7 +24,7 @@ from selenium.webdriver.support.ui import Select, WebDriverWait
 
 from selenium.common.exceptions import NoSuchElementException, StaleElementReferenceException, ElementNotInteractableException
 
-from html_table_parser import parser_functions as parser
+# from html_table_parser import parser_functions as parser
 
 
 #%%
@@ -41,24 +41,30 @@ driver.get(url)
 #%%
 page_path = '//*[@id="main"]/div/div[2]/div/div/div'
 page_length_tmp = driver.find_element_by_xpath(page_path).text
-page_length = int(page_length_tmp.split(' ')[3])
+page_length = int(re.sub(',', '', page_length_tmp.split(' ')[3]))
 
 
-accid_dict = []
+result_ = []
 start = time.time()
 for j in range(1, page_length):
-    accid_num_path = '//*[@id="main"]/div/div[1]/table/tbody'
-    accid_num_tmp = driver.find_element_by_xpath(accid_num_path)
-    accid_num = len(accid_num_tmp.find_elements_by_tag_name('tr'))
+    num_path = '//*[@id="main"]/div/div[1]/table/tbody'
+    num_tmp = driver.find_element_by_xpath(num_path)
+    num = len(num_tmp.find_elements_by_tag_name('tr'))
 
-    row_length = tqdm(range(1, accid_num + 1), file = sys.stdout)
+    row_length = tqdm(range(1, num + 1), file = sys.stdout)
 
     for i in row_length:
+        accid_dict = {}
+        
+        accid_num_path = '//*[@id="main"]/div/div[1]/table/tbody/tr[%d]/td[1]' % i
+        accid_num = driver.find_element_by_xpath(accid_num_path).text
+        accid_dict['accid_num'] = accid_num
+        
         accid_path = '/html/body/div[3]/div[3]/div/div/div[3]/section/form/div/div[1]/table/tbody/tr[%d]/td[2]/a' % i
-        accid_url_path = driver.find_element_by_xpath(accid_path)
-        accid_url = accid_url_path.get_attribute('href')
+        accid_url = driver.find_element_by_xpath(accid_path).get_attribute('href')
         driver.execute_script(accid_url)
-        time.sleep(1)
+        time.sleep(0.5)
+        
         
         # -------------------------------- 사고사례 -------------------------------- #
         tab = driver.find_elements_by_class_name('table.table-bordered')
@@ -69,6 +75,7 @@ for j in range(1, page_length):
         case_val = [x.text for x in case_tab.find_elements_by_class_name('t-left')]
         
         case_tmp = {case_head[i]: case_val[i] for i in range(len(case_head))}
+        accid_dict.update(case_tmp)
         
         # -------------------------------- 현장특성 -------------------------------- #
         char_tab = tab[1]
@@ -77,11 +84,12 @@ for j in range(1, page_length):
         char_val = [x.text for x in char_tab.find_elements_by_class_name('t-left')]
         
         char_tmp = {char_head[i]: char_val[i] for i in range(len(char_head))}
-        case_tmp.update(char_tmp)
+        accid_dict.update(char_tmp)
         
-        accid_dict.append(case_tmp)
+        result_.append(accid_dict)
         
         driver.back()
+        time.sleep(0.5)
         
         row_length.set_postfix({'page': j})
     
@@ -101,10 +109,6 @@ print('\ntime: ', time.time() - start)
 
 
 #%%
-accident = pd.DataFrame(accid_dict)
-accident = accident.sort_values('발생일시', ascending = False).reset_index(drop = True)
+accident = pd.DataFrame(result_)
 
-
-accident.to_csv('accident.csv', header = True, index = False, encoding = 'utf-8-sig')
-
-accident.head(2)
+accident.to_excel('accident.xlsx', header = True, index = False)
